12163a1faea1a2d5af98f4ed5e05ab17d6210578
Isaias Ferreira Dias <isaiasferreiradias@gmail.com>
2021-06-22 09:40:27 -0300

    teste para deploy heroku

--- src/main/java/com/uniacademia/twitter/App.java
+++ src/main/java/com/uniacademia/twitter/App.java
@@ -3,7 +3,9 @@ package com.uniacademia.twitter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication
 @RestController
@@ -13,5 +15,10 @@ public class App {
 	public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
+	
+	@GetMapping("/")
+	public ModelAndView index() {
+		return new ModelAndView("index");
+	}
 	
 }
